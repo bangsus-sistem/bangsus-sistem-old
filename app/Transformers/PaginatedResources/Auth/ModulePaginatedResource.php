ba2cea8d6ac8a96d8cad33e09d67f9e255c31fa6
artevance <agustinus.michael29@gmail.com>
2021-02-26 12:43:01 +0700

    Add package data to module paginated res

--- app/Transformers/PaginatedResources/Auth/ModulePaginatedResource.php
+++ app/Transformers/PaginatedResources/Auth/ModulePaginatedResource.php
@@ -3,6 +3,7 @@
 namespace App\Transformers\PaginatedResources\Auth;
 
 use App\Abstracts\Transformers\PaginatedResource;
+use App\Transformers\SingleResources\Auth\PackageSingleResource;
 
 class ModulePaginatedResource extends PaginatedResource
 {
@@ -14,6 +15,7 @@ class ModulePaginatedResource extends PaginatedResource
     {
         return [
             'id' => $this->id,
+            'package' => new PackageSingleResource($this->package),
             'ref' => $this->ref,
             'name' => $this->name,
         ];
