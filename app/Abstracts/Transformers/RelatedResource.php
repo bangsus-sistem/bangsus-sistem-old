b0bb852b13d843c5a9021565efdd14ae6ad8de40
artevance <agustinus.michael29@gmail.com>
2021-02-26 10:45:40 +0700

    Make transformer abstractions

---
+++ app/Abstracts/Transformers/RelatedResource.php
@@ -0,0 +1,7 @@
+<?php
+
+namespace App\Abstracts\Transformers;
+
+use Illuminate\Http\Resources\Json\JsonResource;
+
+abstract class RelatedResource extends JsonResource {  }
---
+++ app/Abstracts/Transformers/SwiftCollection.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Abstracts\Transformers;
+
+use Illuminate\Http\Resources\Json\ResourceCollection;
+
+abstract class SwiftCollection extends ResourceCollection
+{
+    /**
+     * @param  \Illuminate\Http\Request  $request
+     * @return array
+     */
+    public function toArray($request)
+    {
+        return $this->collection;
+    }
+}
