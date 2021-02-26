c34410a814696eab20ff58129940278ee38d9731
artevance <agustinus.michael29@gmail.com>
2021-02-26 14:51:30 +0700

    Make user and role table

---
+++ database/migrations/2021_02_26_073831_create_users_and_roles.php
@@ -0,0 +1,56 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateUsersAndRoles extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('roles', function (Blueprint $table) {
+            $table->id();
+            $table->code();
+            $table->name();
+            $table->active();
+            $table->note();
+        });
+        Schema::create('users', function (Blueprint $table) {
+            $table->id();
+            $table->username();
+            $table->password();
+            $table->fullName();
+            $table->role();
+            $table->active();
+            $table->note();
+        });
+        Schema::table('roles', function (Blueprint $table) {
+            $table->userTimestamps();
+            $table->userDelete();
+            $table->timestamps();
+            $table->softDeletes();
+        });
+        Schema::table('users', function (Blueprint $table) {
+            $table->userTimestamps();
+            $table->userDelete();
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('users');
+        Schema::dropIfExists('roles');
+    }
+}
