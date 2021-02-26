1d425b3cf76d8557e0a86a06be6e474d1f021a06
artevance <agustinus.michael29@gmail.com>
2021-02-26 12:53:24 +0700

    Make feature single transformers

---
+++ app/Transformers/SingleResources/Auth/FeatureSingleResource.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Transformers\SingleResources\Auth;
+
+use App\Abstracts\Transformers\SingleResource;
+
+class FeatureSingleResource extends SingleResource
+{
+    /**
+     * @param  \Illuminate\Http\Request  $request
+     * @return array
+     */
+    public function toArray($request)
+    {
+        return [
+            'id' => $this->id,
+            'module' => new ModuleSingleResource($this->module),
+            'action' => new ActionSingleResource($this->action),
+        ];
+    }
+}
