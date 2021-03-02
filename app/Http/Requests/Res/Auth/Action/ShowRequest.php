0f580a9b931e9d87d36c7d166e1220caccf422f3
artevance <agustinus.michael29@gmail.com>
2021-03-02 12:08:22 +0700

    Make show request for all endpoints

---
+++ app/Http/Requests/Res/Auth/Action/ShowRequest.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Requests\Res\Auth\Action;
+
+use App\Abstracts\Http\AuthorizedRequest;
+
+class IndexRequest extends AuthorizedRequest
+{
+    /**
+     * @var string
+     */
+    protected $moduleRef = 'action';
+
+    /**
+     * @var string
+     */
+    protected $actionRef = 'read';
+}
---
+++ app/Http/Requests/Res/Auth/Package/ShowRequest.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Requests\Res\Auth\Package;
+
+use App\Abstracts\Http\AuthorizedRequest;
+
+class IndexRequest extends AuthorizedRequest
+{
+    /**
+     * @var string
+     */
+    protected $moduleRef = 'package';
+
+    /**
+     * @var string
+     */
+    protected $actionRef = 'read';
+}
