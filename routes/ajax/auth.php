4e52c17de18fa8bbc25d610344be312fd14e3981
artevance <agustinus.michael29@gmail.com>
2021-03-04 14:28:55 +0700

    Make user and role endpoints routing

--- routes/ajax/auth.php
+++ routes/ajax/auth.php
@@ -36,3 +36,14 @@ Route::prefix('role')->group(function () {
     Route::patch('deactivate', 'RoleController@reviseDeactivate');
     Route::delete('', 'RoleController@destroy');
 });
+
+Route::prefix('user')->group(function () {
+    Route::get('all', 'UserController@manifest');
+    Route::get('', 'UserController@index');
+    Route::get('{id}', 'UserController@show');
+    Route::post('', 'UserController@store');
+    Route::put('', 'UserController@amend');
+    Route::patch('activate', 'UserController@reviseActivate');
+    Route::patch('deactivate', 'UserController@reviseDeactivate');
+    Route::patch('password', 'UserController@revisePassword');
+});
--- routes/api/auth.php
+++ routes/api/auth.php
@@ -25,3 +25,25 @@ Route::prefix('feature')->group(function () {
     Route::get('', 'FeatureController@index');
     Route::get('{id}', 'FeatureController@show');
 });
+
+Route::prefix('role')->group(function () {
+    Route::get('all', 'RoleController@manifest');
+    Route::get('', 'RoleController@index');
+    Route::get('{id}', 'RoleController@show');
+    Route::post('', 'RoleController@store');
+    Route::put('', 'RoleController@amend');
+    Route::patch('activate', 'RoleController@reviseActivate');
+    Route::patch('deactivate', 'RoleController@reviseDeactivate');
+    Route::delete('', 'RoleController@destroy');
+});
+
+Route::prefix('user')->group(function () {
+    Route::get('all', 'UserController@manifest');
+    Route::get('', 'UserController@index');
+    Route::get('{id}', 'UserController@show');
+    Route::post('', 'UserController@store');
+    Route::put('', 'UserController@amend');
+    Route::patch('activate', 'UserController@reviseActivate');
+    Route::patch('deactivate', 'UserController@reviseDeactivate');
+    Route::patch('password', 'UserController@revisePassword');
+});
