1d6a8f11fcaeb47129bad961af891a0ca1d22d47
artevance <agustinus.michael29@gmail.com>
2021-03-08 12:19:18 +0700

    Make authentication log controller

---
+++ app/Http/Controllers/Ajax/Logs/AuthenticationLogController.php
@@ -0,0 +1,146 @@
+<?php
+
+namespace App\Http\Controllers\Ajax\Logs;
+
+use App\Abstracts\Http\Controller;
+use App\Http\Requests\Res\Logs\AuthenticationLog\{
+    IndexRequest,
+    ShowRequest,
+    StoreLoginRequest,
+    StoreTokenRequest,
+    StoreLogoutRequest,
+    AmendRequest,
+    ReviseActivationRequest,
+    DestroyRequest,
+};
+use App\Http\Jobs\Logs\AuthenticationLog\{
+    StoreLoginJob,
+    StoreTokenJob,
+    StoreLogoutJob,
+    AmendJob,
+    ReviseActivationJob,
+    DestroyJob,
+};
+use App\Http\Services\Ajax\{
+    LoginService,
+    TokenService,
+    LogoutService,
+};
+use App\Database\Models\Logs\AuthenticationLog;
+use App\Transformers\SingleCollections\Logs\AuthenticationLogSingleCollection;
+use App\Transformers\PaginatedCollections\Logs\AuthenticationLogPaginatedCollection;
+use App\Transformers\RelatedResources\Logs\AuthenticationLogRelatedResource;
+
+class AuthenticationLogController extends Controller
+{
+    /**
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function manifest()
+    {
+        return response()->json(
+            new AuthenticationLogSingleCollection(AuthenticationLog::isNotHidden()->isActive()->all()),
+            200
+        );
+    }
+
+    /**
+     * @param  \App\Http\Requests\Res\Logs\AuthenticationLog\IndexRequest  $request
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function index(IndexRequest $request)
+    {
+        return response()->json(
+            new AuthenticationLogPaginatedCollection(
+                AuthenticationLog::where(
+                    $this->buildWhere()
+                        ->index('code')->mode('string')
+                        ->index('name')->mode('string')
+                        ->index('active')->mode('boolean')
+                        ->index('locked')->mode('boolean')
+                        ->index('all_access')->mode('boolean')
+                        ->done()
+                )
+                    ->isNotHidden()
+                    ->get()
+            ),
+            200
+        );
+    }
+
+    /**
+     * @param  \App\Http\Requests\Res\Logs\AuthenticationLog\ShowRequest  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function show(ShowRequest $request, int $id)
+    {
+        return response()->json(
+            new AuthenticationLogRelatedResource(AuthenticationLog::isNotHidden()->findOrFail($id)),
+            200
+        );
+    }
+    
+    /**
+     * @param  \App\Http\Requests\Res\Logs\AuthenticationLog\StoreLoginRequest  $request
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function storeLogin(StoreLoginRequest $request)
+    {
+        $user = $this->manage(new LoginService, $request);
+        return response()->json(
+            new AuthenticationLogRelatedResource(
+                $this->dispatch(new StoreLoginJob, $request, $user)
+            ),
+            201
+        );
+    }
+    
+    /**
+     * @param  \App\Http\Requests\Res\Logs\AuthenticationLog\StoreTokenRequest  $request
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function storeToken(StoreTokenRequest $request)
+    {
+        $tokenServiceResult = $this->manage(new TokenService, $request);
+
+        return response()->json(
+            new AuthenticationLogRelatedResource(
+                $this->dispatch(
+                    new StoreTokenJob,
+                    $request,
+                    $tokenServiceResult->user,
+                    $tokenServiceResult->authenticationToken,
+                )
+            ),
+            201
+        );
+    }
+    
+    /**
+     * @param  \App\Http\Requests\Res\Logs\AuthenticationLog\StoreLogoutRequest  $request
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function storeLogout(StoreLogoutRequest $request)
+    {
+        $user = $this->manage(new LogoutService, $request);
+        return response()->json(
+            new AuthenticationLogRelatedResource(
+                $this->dispatch(new StoreLogoutJob, $request, $user)
+            ),
+            201
+        );
+    }
+
+    /**
+     * @param  \App\Http\Requests\Res\Logs\AuthenticationLog\DestroyRequest  $request
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function destroy(DestroyRequest $request)
+    {
+        return response()->json(
+            $this->dispatch(new DestroyJob, $request),
+            204
+        );
+    }
+}
