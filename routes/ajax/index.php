2a71787574f035010346b0df048f32611965849f
artevance <agustinus.michael29@gmail.com>
2021-03-08 14:33:30 +0700

    Fix bug in routing

--- routes/ajax/index.php
+++ routes/ajax/index.php
@@ -3,7 +3,7 @@
 use Illuminate\Support\Facades\Route;
 
 // Route with none auth guard.
-Route::group(base_path('routes/ajax/misc.php'));
+Route::prefix('')->group(base_path('routes/ajax/misc.php'));
 
 // Route with sanctum auth middleware.
 Route::middleware('auth:sanctum')->group(function () {
