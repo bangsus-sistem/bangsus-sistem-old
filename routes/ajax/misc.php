ecbf675a4e44de0d80af46e5247ecbb782484676
artevance <agustinus.michael29@gmail.com>
2021-03-08 19:10:13 +0700

    Adding namespacing to avoid not found exception

--- routes/ajax/misc.php
+++ routes/ajax/misc.php
@@ -2,7 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::prefix('authentication_log')->group(function () {
-    Route::post('login', 'AuthenticationLogController@storeLogin');
-    Route::post('token', 'AuthenticationLogController@storeToken');
+Route::prefix('logs/authentication_log')->group(function () {
+    Route::post('login', 'Logs\AuthenticationLogController@storeLogin');
+    Route::post('token', 'Logs\AuthenticationLogController@storeToken');
 });
