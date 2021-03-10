fb94c746ea7ec852a306963e7aa51dfb8480fe33
artevance <agustinus.michael29@gmail.com>
2021-03-10 11:54:36 +0700

    Make module seeder

---
+++ database/seeders/Injections/ModuleSeeder.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace Database\Seeders\Injections;
+
+use Illuminate\Database\Seeder;
+
+class ModuleSeeder extends Seeder
+{
+    /**
+     * Seed the application's database.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $data = $this->parseData([
+            'auth' => [
+                ['id' => '0001', 'ref' => 'package', 'name' => 'Package'],
+                ['id' => '0002', 'ref' => 'module', 'name' => 'Modul'],
+                ['id' => '0003', 'ref' => 'action', 'name' => 'Aksi'],
+                ['id' => '0004', 'ref' => 'feature', 'name' => 'Fitur'],
+                ['id' => '0005', 'ref' => 'role', 'name' => 'Role'],
+                ['id' => '0006', 'ref' => 'user', 'name' => 'User'],
+                ['id' => '0007', 'ref' => 'authentication_token', 'name' => 'Token Autentikasi'],
+                ['id' => '0008', 'ref' => 'api_token', 'name' => 'Token API'],
+            ],
+            'logs' => [
+                ['id' => '0001', 'ref' => 'authentication_log', 'name' => 'Log Autentikasi'],
+                ['id' => '0002', 'ref' => 'feature_log', 'name' => 'Log Fitur'],
+            ],
+            'system' => [
+                ['id' => '0001', 'ref' => 'branch_type', 'name' => 'Tipe Cabang'],
+                ['id' => '0002', 'ref' => 'branch', 'name' => 'Cabang'],
+            ]
+        ]);
+        \DB::table('module')->insert($data);
+    }
+
+    /**
+     * @param  array  $data
+     * @return array
+     */
+    private function parseData($data)
+    {
+        $return = [];
+        foreach ($data as $packageRef => $module) {
+            $package = \DB::table('package')->where('ref', $packageRef)->first();
+            $return[] = [
+                'id' => (int) ((string) $package->id . $module['id']),
+                'package_id' => $package->id,
+                'ref' => $module->ref,
+                'name' => $module->name,
+            ];
+        }
+        return $return;
+    }
+}
