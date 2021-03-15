052037fdd9bc5401e112649041d45c85d6259462
artevance <agustinus.michael29@gmail.com>
2021-03-15 11:55:16 +0700

    Make version checker utils

---
+++ app/Http/Controllers/Ajax/Utils/VersionCheckController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Controllers\Ajax\Utils;
+
+use App\Abstracts\Http\Controller;
+use Illuminate\Http\Request;
+
+class VersionCheckController extends Controller
+{
+    /**
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function index(Request $request)
+    {
+        return response()->json([
+            'app_version' => config('app.version')
+        ]);
+    }
+}
--- routes/ajax/utils.php
+++ routes/ajax/utils.php
@@ -3,3 +3,4 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('authenticated', 'AuthenticatedController@index');
+Route::get('version_check', 'VersionCheckController@index');
