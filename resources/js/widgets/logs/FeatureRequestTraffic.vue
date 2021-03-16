07a44ea0f73f7e1903687fc9e8baa5f8daeeca01
artevance <agustinus.michael29@gmail.com>
2021-03-16 15:37:52 +0700

    Fix widget of feature request traffic

--- resources/js/widgets/logs/FeatureRequestTraffic.vue
+++ resources/js/widgets/logs/FeatureRequestTraffic.vue
@@ -1,36 +1,36 @@
 <template>
     <div class="col-4">
-        <div class="card shadow-sm">
-            <div class="card-body">
-                <h6 class="card-title">Traffic Request Fitur</h6>
-                    <div class="form-group">
-                        <select class="form-control form-control-sm">
-                            <option>5 menit terakhir</option>
-                            <option>15 menit terakhir</option>
-                            <option>1 jam terakhir</option>
-                            <option>2 jam terakhir</option>
-                            <option>6 jam terakhir</option>
-                            <option>12 jam terakhir</option>
-                            <option>24 jam terakhir</option>
-                            <option>3 hari terakhir</option>
-                            <option>1 minggu terakhir</option>
-                        </select>
+        <bsb-card>
+            <bsb-card-body>
+                <h5 class="card-title">Traffic Request Fitur</h5>
+                <div class="form-group mt-4">
+                    <select class="form-control form-control-sm">
+                        <option>5 menit terakhir</option>
+                        <option>15 menit terakhir</option>
+                        <option>1 jam terakhir</option>
+                        <option>2 jam terakhir</option>
+                        <option>6 jam terakhir</option>
+                        <option>12 jam terakhir</option>
+                        <option>24 jam terakhir</option>
+                        <option>3 hari terakhir</option>
+                        <option>1 minggu terakhir</option>
+                    </select>
+                </div>
+                <bsb-row>
+                    <div class="col text-center">
+                        <h5 class="text-green-500">20</h5>
+                        <span class="text-green-500">Permintaan</span>
                     </div>
-                    <div class="row">
-                        <div class="col text-center">
-                            <h5 class="text-green-500">20</h5>
-                            <span class="text-green-500">Permintaan</span>
-                        </div>
-                        <div class="col text-center">
-                            <h5 class="text-red-400">5</h5>
-                            <span class="text-red-400">Fitur Diakses</span>
-                        </div>
-                        <div class="col text-center">
-                            <h5 class="text-yellow-600">2</h5>
-                            <span class="text-yellow-600">Pengguna</span>
-                        </div>
+                    <div class="col text-center">
+                        <h5 class="text-red-400">5</h5>
+                        <span class="text-red-400">Fitur Diakses</span>
                     </div>
-            </div>
-        </div>
+                    <div class="col text-center">
+                        <h5 class="text-yellow-600">2</h5>
+                        <span class="text-yellow-600">Pengguna</span>
+                    </div>
+                </bsb-row>
+            </bsb-card-body>
+        </bsb-card>
     </div>
 </template>
