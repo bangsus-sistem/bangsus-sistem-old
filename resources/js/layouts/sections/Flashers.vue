8797514c2b1609aafd669735ea0f343c37805416
artevance <agustinus.michael29@gmail.com>
2021-03-16 14:57:39 +0700

    Make flashers section

---
+++ resources/js/layouts/sections/Flashers.vue
@@ -0,0 +1,21 @@
+<template>
+    <bsb-flashers :flashers="flashers" @closed="closeFlasher($event)" />
+</template>
+
+<script>
+export default {
+    computed: {
+        flashers() {
+            return this.$store.getters['utils/flashers/flashers']
+        },
+    },
+    methods: {
+        closeFlasher(id) {
+            this.$store.dispatch('utils/flashers/closeFlasher', id)
+        }
+    },
+    created() {
+        this.$store.dispatch('utils/flashers/buildStoredFlashersTimeout')
+    },
+}
+</script>
