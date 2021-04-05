02e85f81241545936687260697684373ae418b8c
artevance <agustinus.michael29@gmail.com>
2021-04-05 08:59:40 +0700

    Make api routes

--- routes/api/index.php
+++ routes/api/index.php
@@ -1,5 +1,20 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+
+// Route with none auth guard.
+Route::prefix('')->group(base_path('routes/api/misc.php'));
     
-Route::prefix('auth')->namespace('Auth')->group(base_path('routes/api/auth.php'));
+// Route with API auth middleware.
+Route::middleware('auth.api')->group(function () {
+
+    /**
+     * @package \App\Http\Controllers\Api\Auth
+     */
+    Route::prefix('auth')->namespace('Auth')->group(base_path('routes/api/auth.php'));
+
+    /**
+     * @package \App\Http\Controllers\Api\Logs
+     */
+    Route::prefix('log')->namespace('Log')->group(base_path('routes/api/log.php'));
+});
