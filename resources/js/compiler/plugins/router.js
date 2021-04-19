604008359c972429ca05b26332da27b0335069b2
artevance <agustinus.michael29@gmail.com>
2021-04-19 13:43:13 +0700

    Make back button work and in-depth query

--- resources/js/compiler/plugins/router.js
+++ resources/js/compiler/plugins/router.js
@@ -1,9 +1,17 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import routes from '../../routes'
+import qs from 'qs'
 
 Vue.use(VueRouter)
 export default new VueRouter({
     mode: 'history',
     routes,
+    parseQuery(query) {
+        return qs.parse(query);
+    },
+    stringifyQuery(query) {
+        let result = qs.stringify(query);
+        return result ? ('?' + result) : '';
+    }
 })
