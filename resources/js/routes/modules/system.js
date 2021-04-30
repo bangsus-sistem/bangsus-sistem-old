b55358a64e9a67889fc917fb2eba5c4ec1f91cf7
artevance <agustinus.michael29@gmail.com>
2021-04-30 12:54:57 +0700

    Add some role routes

--- resources/js/routes/modules/system.js
+++ resources/js/routes/modules/system.js
@@ -82,4 +82,22 @@ export default [
         component: page('System/Role/Index'),
         beforeEnter: middleware(appMiddleware),
     },
+    {
+        path: 'role/create',
+        name: 'system.role.create',
+        component: page('System/Role/Create'),
+        beforeEnter: middleware(appMiddleware),
+    },
+    {
+        path: 'role/read/:id',
+        name: 'system.role.read',
+        component: page('System/Role/Read'),
+        beforeEnter: middleware(appMiddleware),
+    },
+    {
+        path: 'role/update/:id',
+        name: 'system.role.update',
+        component: page('System/Role/Update'),
+        beforeEnter: middleware(appMiddleware),
+    },
 ];
