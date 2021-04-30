999ef07a19f5623119188bb12fd500bf953eb652
artevance <agustinus.michael29@gmail.com>
2021-04-30 12:49:16 +0700

    Add new slot to list group item content

--- resources/js/components/list/ListGroupItemContent.vue
+++ resources/js/components/list/ListGroupItemContent.vue
@@ -1,10 +1,17 @@
 <template>
-    <div class="d-flex justify-content-between">
-        <div class="col p-0 m-0 text-truncate">
-            <slot name="content" />
+    <div>
+        <div class="row justify-content-between mx-1">
+            <div class="col p-0 m-0 text-truncate">
+                <slot name="content" />
+            </div>
+            <div class="col p-0 my-auto text-right align-items-middle">
+                <slot name="right" />
+            </div>
         </div>
-        <div class="col p-0 my-auto text-right align-items-middle">
-            <slot name="right" />
+        <div class="row mx-1">
+            <div class="col p-0">
+                <slot name="footer" />
+            </div>
         </div>
     </div>
 </template>
