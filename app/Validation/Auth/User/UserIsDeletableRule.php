a148d0fe4fc9fb51ca9dd883b33ea16d23000a83
artevance <agustinus.michael29@gmail.com>
2021-05-03 15:47:19 +0700

    Make user is deletable rule

---
+++ app/Validation/Auth/User/UserIsDeletableRule.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Validation\Auth\User;
+
+use App\Foundation\Validation\RequestRule;
+use Illuminate\Contracts\Validation\Rule;
+use App\Models\Auth\User;
+
+class UserIsDeletableRule extends RequestRule implements Rule
+{
+    /**
+     * @param  string  $attribute
+     * @param  mixed  $value
+     * @return bool
+     */
+    public function passes($attribute, $value)
+    {
+        $user = User::findOrFail($this->request->input('id'));
+        if ($user->locked) {
+            $this->setMessage('User tidak boleh dihapus.');
+            return false;
+        }
+        if ($user->users()->exists()) {
+            $this->setMessage('User sudah memiliki user.');
+            return false;
+        }
+
+        return true;
+    }
+}
