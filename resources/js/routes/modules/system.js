ebf8fe5b8d2c306f578329bcaa9d3d596fbf00f6
artevance <agustinus.michael29@gmail.com>
2021-05-16 11:30:14 +0700

    Make route for user

--- resources/js/routes/modules/system.js
+++ resources/js/routes/modules/system.js
@@ -100,4 +100,28 @@ export default [
         component: page('System/Role/Update'),
         beforeEnter: middleware(appMiddleware),
     },
+    {
+        path: 'user',
+        name: 'system.user',
+        component: page('System/User/Index'),
+        beforeEnter: middleware(appMiddleware),
+    },
+    // {
+    //     path: 'user/create',
+    //     name: 'system.user.create',
+    //     component: page('System/User/Create'),
+    //     beforeEnter: middleware(appMiddleware),
+    // },
+    // {
+    //     path: 'user/read/:id',
+    //     name: 'system.user.read',
+    //     component: page('System/User/Read'),
+    //     beforeEnter: middleware(appMiddleware),
+    // },
+    // {
+    //     path: 'user/update/:id',
+    //     name: 'system.user.update',
+    //     component: page('System/User/Update'),
+    //     beforeEnter: middleware(appMiddleware),
+    // },
 ];
