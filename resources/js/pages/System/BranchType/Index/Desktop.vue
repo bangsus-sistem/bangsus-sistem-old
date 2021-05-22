4a8bfd5219add681d517b61ec108e0098fac664b
artevance <agustinus.michael29@gmail.com>
2021-05-22 15:01:17 +0700

    Make branch type index page

---
+++ resources/js/pages/System/BranchType/Index/Desktop.vue
@@ -0,0 +1,71 @@
+<template>
+    <fragment>
+        <h3>Tipe Cabang</h3>
+        <bsb-card class="my-3">
+            <bsb-card-body-spinner-error
+                :loading="state.page.loading"
+                :error="state.page.error"
+                :error-message="state.page.message"
+            >
+                <h5 class="mb-3">Daftar Tipe Cabang</h5>
+                <bsb-access-wrapper module-ref="branch_type" action-ref="create">
+                    <bsb-button-router-link-create :to="{ name: 'system.branchType.create' }" />
+                </bsb-access-wrapper>
+                <bsb-table-responsive class="p-1 mt-3">
+                    <bsb-table-responsive-header>
+                        <bsb-item-count :options="meta.counts" v-model="query.count" @input="search" />
+                    </bsb-table-responsive-header>
+                    <bsb-table :hover="true">
+                        <thead class="thead-light">
+                            <BranchTypeDataQuery
+                                :loading="state.result.loading"
+                                @search="search"
+                                v-model="query"
+                            />
+                            <bsb-tr-sort
+                                :sort-orders="meta.sortOrders"
+                                :sort="query['sort']"
+                                :order="query['order']"
+                                v-model="query"
+                                @sort="changeSortOrder"
+                            />
+                        </thead>
+                        <bsb-tbody-empty :items="result.items" :col="meta.sortOrders.length">
+                            <BranchTypeDataRow
+                                v-for="(item, i) in result.items"
+                                :key="i"
+                                :num="i + 1"
+                                :item="item"
+                                @activate="showModalForm('role', 'activate', { id: item['id'] })"
+                                @deactivate="showModalForm('role', 'deactivate', { id: item['id'] })"
+                                @delete="showModalForm('role', 'delete', { id: item['id'] })"
+                            />
+                        </bsb-tbody-empty>
+                    </bsb-table>
+                    <bsb-table-responsive-footer>
+                        <bsb-data-index
+                            :first-item="result.meta['first_item']"
+                            :last-item="result.meta['last_item']"
+                            :total="result.meta['total']"
+                        />
+                        <bsb-page-button-group v-model="query.page" :last-page="result.meta['last_page']" @changed="search" />
+                    </bsb-table-responsive-footer>
+                </bsb-table-responsive>
+            </bsb-card-body-spinner-error>
+        </bsb-card>
+        <!-- Modal Form -->
+        <BranchTypeModalForms ref="role" @success="search" />
+    </fragment>
+</template>
+
+<script>
+import mixin from './mixin'
+import BranchTypeDataQuery from './DataQuery'
+import BranchTypeDataRow from './DataRow'
+import BranchTypeModalForms from './ModalForms'
+
+export default {
+    mixins: [mixin],
+    components: { BranchTypeDataQuery, BranchTypeDataRow, BranchTypeModalForms },
+}
+</script>
---
+++ resources/js/pages/System/BranchType/Index/ModalForms.vue
@@ -0,0 +1,32 @@
+<template>
+    <fragment>
+        <bsb-access-wrapper module-ref="branch_type" action-ref="update">
+            <bsb-modal-form
+                title="Aktifkan Tipe Cabang"
+                message="Apakah anda yakin?"
+                ref="activate"
+                link="/ajax/system/branch_type/activate"
+                method="patch"
+                @success="$emit('success')"
+            />
+            <bsb-modal-form
+                title="Nonaktifkan Tipe Cabang"
+                message="Apakah anda yakin?"
+                ref="deactivate"
+                link="/ajax/system/branch_type/deactivate"
+                method="patch"
+                @success="$emit('success')"
+            />
+        </bsb-access-wrapper>
+        <bsb-access-wrapper module-ref="branchType" action-ref="delete">
+            <bsb-modal-form
+                title="Hapus Tipe Cabang"
+                message="Apakah anda yakin?"
+                ref="delete"
+                link="/ajax/system/branch_type"
+                method="delete"
+                @success="$emit('success')"
+            />
+        </bsb-access-wrapper>
+    </fragment>
+</template>
