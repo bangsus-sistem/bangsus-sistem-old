f680aa87daaa8c64730402e10e22af3fb27fa477
artevance <agustinus.michael29@gmail.com>
2021-06-03 16:03:16 +0700

    Fix branch type data row

--- resources/js/pages/System/BranchType/Index/DataRow.vue
+++ resources/js/pages/System/BranchType/Index/DataRow.vue
@@ -27,28 +27,30 @@
         </template>
         <template v-slot:mobile>
             <bsb-list-group-item>
-                <template v-slot:content>
-                    <small>{{ item['code'] }}</small>
-                    <h6>{{ item['name'] }}</h6>
-                </template>
-                <template v-slot:right>
-                    <bsb-access-wrapper module-ref="branch_type" action-ref="read">
-                        <bsb-button-router-link-read :to="{ name: 'system.branchType.read', params: { id: item['id'] } }" />
-                    </bsb-access-wrapper>
-                    <bsb-access-wrapper module-ref="branch_type" action-ref="update">
-                        <bsb-button-router-link-update :to="{ name: 'system.branchType.update', params: { id: item['id'] } }" v-if="!item['locked']" />
-                        <template v-if="!item['locked']">
-                            <bsb-button-activate v-if="!item['active']" @click="$emit('activate')" />
-                            <bsb-button-deactivate v-else @click="$emit('deactivate')" />
-                        </template>
-                    </bsb-access-wrapper>
-                    <bsb-access-wrapper module-ref="branch_type" action-ref="delete">
-                        <bsb-button-delete @click="$emit('delete')" v-if="!item['locked']" />
-                    </bsb-access-wrapper>
-                </template>
-                <template v-slot:footer>
-                    <bsb-switch-badge :condition="item['active']" true-label="Aktif" false-label="Tidak Aktif"/>
-                </template>
+                <bsb-list-group-item-content>
+                    <template v-slot:content>
+                        <small>{{ item['code'] }}</small>
+                        <h6>{{ item['name'] }}</h6>
+                    </template>
+                    <template v-slot:right>
+                        <bsb-access-wrapper module-ref="branch_type" action-ref="read">
+                            <bsb-button-router-link-read :to="{ name: 'system.branchType.read', params: { id: item['id'] } }" />
+                        </bsb-access-wrapper>
+                        <bsb-access-wrapper module-ref="branch_type" action-ref="update">
+                            <bsb-button-router-link-update :to="{ name: 'system.branchType.update', params: { id: item['id'] } }" v-if="!item['locked']" />
+                            <template v-if="!item['locked']">
+                                <bsb-button-activate v-if="!item['active']" @click="$emit('activate')" />
+                                <bsb-button-deactivate v-else @click="$emit('deactivate')" />
+                            </template>
+                        </bsb-access-wrapper>
+                        <bsb-access-wrapper module-ref="branch_type" action-ref="delete">
+                            <bsb-button-delete @click="$emit('delete')" v-if="!item['locked']" />
+                        </bsb-access-wrapper>
+                    </template>
+                    <template v-slot:footer>
+                        <bsb-switch-badge :condition="item['active']" true-label="Aktif" false-label="Tidak Aktif"/>
+                    </template>
+                </bsb-list-group-item-content>
             </bsb-list-group-item>
         </template>
     </bsb-screen>
