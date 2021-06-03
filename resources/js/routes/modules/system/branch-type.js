460a16ac84d5bf5be39269df562718328bbd9133
artevance <agustinus.michael29@gmail.com>
2021-06-03 16:09:29 +0700

    Make branch and branch type routes

--- resources/js/routes/modules/system/branch-type.js
+++ resources/js/routes/modules/system/branch-type.js
@@ -9,22 +9,22 @@ export default [
         component: page('System/BranchType/Index'),
         beforeEnter: middleware(appMiddleware),
     },
-    // {
-    //     path: 'branch_type/create',
-    //     name: 'system.branchType.create',
-    //     component: page('System/BranchType/Create'),
-    //     beforeEnter: middleware(appMiddleware),
-    // },
-    // {
-    //     path: 'branch_type/read/:id',
-    //     name: 'system.branchType.read',
-    //     component: page('System/BranchType/Read'),
-    //     beforeEnter: middleware(appMiddleware),
-    // },
-    // {
-    //     path: 'branch_type/update/:id',
-    //     name: 'system.branchType.update',
-    //     component: page('System/BranchType/Update'),
-    //     beforeEnter: middleware(appMiddleware),
-    // },
+    {
+        path: 'branch_type/create',
+        name: 'system.branchType.create',
+        component: page('System/BranchType/Create'),
+        beforeEnter: middleware(appMiddleware),
+    },
+    {
+        path: 'branch_type/read/:id',
+        name: 'system.branchType.read',
+        component: page('System/BranchType/Read'),
+        beforeEnter: middleware(appMiddleware),
+    },
+    {
+        path: 'branch_type/update/:id',
+        name: 'system.branchType.update',
+        component: page('System/BranchType/Update'),
+        beforeEnter: middleware(appMiddleware),
+    },
 ]
--- resources/js/routes/modules/system/branch.js
+++ resources/js/routes/modules/system/branch.js
@@ -9,22 +9,22 @@ export default [
         component: page('System/Branch/Index'),
         beforeEnter: middleware(appMiddleware),
     },
-    // {
-    //     path: 'branch/create',
-    //     name: 'system.branch.create',
-    //     component: page('System/Branch/Create'),
-    //     beforeEnter: middleware(appMiddleware),
-    // },
-    // {
-    //     path: 'branch/read/:id',
-    //     name: 'system.branch.read',
-    //     component: page('System/Branch/Read'),
-    //     beforeEnter: middleware(appMiddleware),
-    // },
-    // {
-    //     path: 'branch/update/:id',
-    //     name: 'system.branch.update',
-    //     component: page('System/Branch/Update'),
-    //     beforeEnter: middleware(appMiddleware),
-    // },
+    {
+        path: 'branch/create',
+        name: 'system.branch.create',
+        component: page('System/Branch/Create'),
+        beforeEnter: middleware(appMiddleware),
+    },
+    {
+        path: 'branch/read/:id',
+        name: 'system.branch.read',
+        component: page('System/Branch/Read'),
+        beforeEnter: middleware(appMiddleware),
+    },
+    {
+        path: 'branch/update/:id',
+        name: 'system.branch.update',
+        component: page('System/Branch/Update'),
+        beforeEnter: middleware(appMiddleware),
+    },
 ]
