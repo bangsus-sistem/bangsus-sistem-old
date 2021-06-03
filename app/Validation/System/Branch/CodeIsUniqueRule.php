85afbdf28d10c23b80a96f298b4891b02eb3181f
artevance <agustinus.michael29@gmail.com>
2021-06-03 15:18:52 +0700

    Make rule objects

---
+++ app/Validation/System/Branch/CodeIsUniqueRule.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Validation\System\Branch;
+
+use App\Foundation\Validation\RequestRule;
+use Illuminate\Contracts\Validation\Rule;
+use App\Models\System\{
+    Branch,
+    BranchType,
+};
+
+class CodeIsUniqueRule extends RequestRule implements Rule
+{
+    /**
+     * @param  string  $attribute
+     * @param  mixed  $value
+     * @return bool
+     */
+    public function passes($attribute, $value)
+    {
+        $branchType = BranchType::find($this->request->input('branch_type_id'));
+        $code = $branchType->code . $value;
+
+        if ( ! Branch::where('code', $code)->exists()) {
+            $this->setMessage('Kode Cabang sudah dipakai.' .);
+            return false;
+        }
+
+        return true;
+    }
+}
