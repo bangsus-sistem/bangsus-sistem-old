23567adf2170e2638c380a1340f7a3d5a2313ef2
artevance <agustinus.michael29@gmail.com>
2021-06-04 12:34:12 +0700

    Adding branch table to branch type read page

--- resources/js/pages/System/BranchType/Read/Mobile.vue
+++ resources/js/pages/System/BranchType/Read/Mobile.vue
@@ -29,10 +29,58 @@
                         <label>Catatan</label>
                         <bsb-textarea v-model="form.data['note']" :readonly="true" />
                     </bsb-form-group>
-                    <bsb-button-spinner type="submit" :loading="state.form.loading">
-                        Submit
-                    </bsb-button-spinner>
                 </form>
+                <!-- Branch -->
+                <bsb-access-wrapper module-ref="branch" action-ref="index">
+                    <div>
+                        <label @click.prevent="toggleShow('branch')">
+                            Branch <bsb-switch-icon true-icon="angle-down" false-icon="angle-up" v-model="meta['branch'].show" :single-color="true" />
+                        </label>
+                        <bsb-table-responsive class="p-1 my-3" v-if="meta['branch'].show">
+                            <bsb-table-responsive-header>
+                                <bsb-item-count :options="meta['branch'].counts" v-model="query['branch'].count" @input="search('branch')" />
+                            </bsb-table-responsive-header>
+                            <bsb-table :hover="true">
+                                <thead class="thead-light">
+                                    <BranchDataQuery
+                                        :loading="state.result['branch'].loading"
+                                        @search="search('branch')"
+                                        v-model="query['branch']"
+                                        :fl-with-branch-type="false"
+                                    />
+                                    <bsb-tr-sort
+                                        :sort-orders="meta['branch'].sortOrders"
+                                        :sort="query['branch']['sort']"
+                                        :order="query['branch']['order']"
+                                        v-model="query['branch']"
+                                        @sort="changeSortOrder($event, true, 'branch')"
+                                    />
+                                </thead>
+                                <bsb-tbody-empty :items="result['branch'].items" :col="meta['branch'].sortOrders.length">
+                                    <BranchDataRow
+                                        v-for="(item, i) in result['branch'].items"
+                                        :key="i"
+                                        :num="i + 1"
+                                        :item="item"
+                                        @activate="showModalForm('branch', 'activate', { id: item['id'] })"
+                                        @deactivate="showModalForm('branch', 'deactivate', { id: item['id'] })"
+                                        @delete="showModalForm('branch', 'delete', { id: item['id'] })"
+                                        :fl-with-branch-type="false"
+                                    />
+                                </bsb-tbody-empty>
+                            </bsb-table>
+                            <bsb-table-responsive-footer>
+                                <bsb-data-index
+                                    :first-item="result['branch'].meta['first_item']"
+                                    :last-item="result['branch'].meta['last_item']"
+                                    :total="result['branch'].meta['total']"
+                                />
+                                <bsb-page-button-group v-model="query['branch'].page" :last-page="result['branch'].meta['last_page']" @changed="search('branch')" />
+                            </bsb-table-responsive-footer>
+                        </bsb-table-responsive>
+                    </div>
+                </bsb-access-wrapper>
+                <!-- End Branch -->
             </bsb-card-body-spinner-error-back>
         </bsb-card>
     </fragment>
