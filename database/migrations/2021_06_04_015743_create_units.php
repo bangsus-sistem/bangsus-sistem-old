2129d169dc96ceb02404248e722ecb71fe830b67
artevance <agustinus.michael29@gmail.com>
2021-06-04 12:32:12 +0700

    Make unit migration

---
+++ database/migrations/2021_06_04_015743_create_units.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateUnits extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('units', function (Blueprint $table) {
+            $table->id();
+            $table->code();
+            $table->name();
+            $table->active();
+            $table->description();
+            $table->note();
+            $table->userTimestamps();
+            $table->userDelete();
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('units');
+    }
+}
