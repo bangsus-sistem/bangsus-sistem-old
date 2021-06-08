d6e0f6ad53e545271e35c89992b9afb2e2c90f33
artevance <agustinus.michael29@gmail.com>
2021-06-08 12:23:23 +0700

    Make division model

---
+++ app/Models/Hrm/Division.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Models\Hrm;
+
+use Illuminate\Database\Eloquent\{
+    Model,
+    SoftDeletes,
+};
+use App\Models\Concerns\{
+    HasUserTimestamps,
+    HasUserDelete,
+    ActiveFlag,
+};
+
+class Divison extends Model
+{
+    use SoftDeletes, HasUserTimestamps, HasUserDelete, ActiveFlag;
+
+    /**
+     * @return \Illuminate\Database\Eloquent\Relations\HasMany
+     */
+    public function jobTitles()
+    {
+        return $this->hasMany(JobTitle::class);
+    }
+}
