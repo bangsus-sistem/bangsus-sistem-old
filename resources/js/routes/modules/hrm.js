843318565fe4f585abaef7ff53bd1e2316863710
artevance <agustinus.michael29@gmail.com>
2021-06-09 15:44:12 +0700

    Include new hrm routing

--- resources/js/routes/modules/hrm.js
+++ resources/js/routes/modules/hrm.js
@@ -1,9 +1,17 @@
 import contactType from './hrm/contact-type';
 import addressType from './hrm/address-type';
 import employeePhotoType from './hrm/employee-photo-type';
+import gender from './hrm/gender.js';
+import division from './hrm/division';
+import jobTitle from './hrm/job-title';
+import attendanceType from './hrm/attendance-type';
 
 export default [
     ...contactType,
     ...addressType,
     ...employeePhotoType,
+    ...gender,
+    ...division,
+    ...jobTitle,
+    ...attendanceType,
 ];
