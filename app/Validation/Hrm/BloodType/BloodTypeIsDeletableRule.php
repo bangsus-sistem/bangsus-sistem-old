9e2dc456729726b288fe1a748536342ff9fc7676
artevance <agustinus.michael29@gmail.com>
2021-06-14 12:47:51 +0700

    Make blood type validation

---
+++ app/Validation/Hrm/BloodType/BloodTypeIsDeletableRule.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Validation\Hrm\BloodType;
+
+use App\Foundation\Validation\RequestRule;
+use Illuminate\Contracts\Validation\Rule;
+use App\Models\Hrm\BloodType;
+
+class BloodTypeIsDeletableRule extends RequestRule implements Rule
+{
+    /**
+     * @param  string  $attribute
+     * @param  mixed  $value
+     * @return bool
+     */
+    public function passes($attribute, $value)
+    {
+        $bloodType = BloodType::findOrFail(
+            $this->request->input('id')
+        );
+        // if ($bloodType->items()->exists()) {
+        //     $this->setMessage('');
+        //     return false;
+        // }
+
+        return true;
+    }
+}
