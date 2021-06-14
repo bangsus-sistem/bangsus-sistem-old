551c709bd6b8a57ecbf9d4e1b6cac3221df874c0
artevance <agustinus.michael29@gmail.com>
2021-06-14 12:44:11 +0700

    Make blood type migration

---
+++ database/migrations/2021_06_10_013338_create_blood_types.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateBloodTypes extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('blood_types', function (Blueprint $table) {
+            $table->id();
+            $table->name();
+            $table->active();
+            $table->description();
+            $table->note();
+            $table->userTimestamps();
+            $table->userDelete();
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('blood_types');
+    }
+}
