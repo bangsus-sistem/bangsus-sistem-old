4ff8fc514785ee36673b7760cf585c847d4458e7
artevance <agustinus.michael29@gmail.com>
2021-06-23 15:49:23 +0700

    Reshape observer kernel

--- app/Observers/Kernel.php
+++ app/Observers/Kernel.php
@@ -8,10 +8,6 @@ class Kernel
      * @var array
      */
     public static $observers = [
-        \App\Models\Auth\Role::class => Auth\RoleObserver::class,
-        \App\Models\Auth\User::class => Auth\UserObserver::class,
-        \App\Models\System\Branch::class => System\BranchObserver::class,
-        \App\Models\System\BranchType::class => System\BranchTypeObserver::class,
         \App\Models\Hrm\Employee::class => Hrm\EmployeeObserver::class,
     ];
 }
