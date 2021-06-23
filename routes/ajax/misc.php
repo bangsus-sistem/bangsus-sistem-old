1967b064a93ecb1bda9a40d2e9b85bb41bd3d67d
artevance <agustinus.michael29@gmail.com>
2021-06-23 15:56:02 +0700

    Delete unused routes

--- routes/ajax/misc.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-use Illuminate\Support\Facades\Route;
-
-Route::prefix('log/authentication_log')->group(function () {
-    Route::post('login', 'Log\AuthenticationLogController@storeLogin');
-    Route::post('authentication_token', 'Log\AuthenticationLogController@storeAuthenticationToken');
-});
--- routes/api/auth.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-use Illuminate\Support\Facades\Route;
-
-Route::prefix('package')->group(function () {
-    Route::get('all', 'PackageController@manifest');
-    Route::get('', 'PackageController@index');
-    Route::get('{id}', 'PackageController@show');
-});
