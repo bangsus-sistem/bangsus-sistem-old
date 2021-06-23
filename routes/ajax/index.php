00af67e60bf2a34924917b8316e83c15d664ef0f
artevance <agustinus.michael29@gmail.com>
2021-06-23 15:56:46 +0700

    Change the routing indexes

--- routes/ajax/index.php
+++ routes/ajax/index.php
@@ -2,31 +2,8 @@
 
 use Illuminate\Support\Facades\Route;
 
-// Route with none auth guard.
-Route::prefix('')->group(base_path('routes/ajax/misc.php'));
-
-/**
- * @package \App\Http\Controllers\Ajax\Utils
- */
-Route::prefix('utils')->namespace('Utils')->group(base_path('routes/ajax/utils.php'));
-
 // Route with sanctum auth middleware.
 Route::middleware('auth:sanctum')->group(function () {
-
-    /**
-     * @package \App\Http\Controllers\Ajax\Auth
-     */
-    Route::prefix('auth')->namespace('Auth')->group(base_path('routes/ajax/auth.php'));
-
-    /**
-     * @package \App\Http\Controllers\Ajax\Logs
-     */
-    Route::prefix('log')->namespace('Log')->group(base_path('routes/ajax/log.php'));
-
-    /**
-     * @package \App\Http\Controllers\Ajax\System
-     */
-    Route::prefix('system')->namespace('System')->group(base_path('routes/ajax/system.php'));
 
     /**
      * @package \App\Http\Controllers\Ajax\Master
--- routes/api/index.php
+++ routes/api/index.php
@@ -1,27 +1,9 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
-// Route with none auth guard.
-Route::prefix('')->group(base_path('routes/api/misc.php'));
     
 // Route with API auth middleware.
 Route::middleware('auth.api')->group(function () {
-
-    /**
-     * @package \App\Http\Controllers\Api\Auth
-     */
-    Route::prefix('auth')->namespace('Auth')->group(base_path('routes/api/auth.php'));
-
-    /**
-     * @package \App\Http\Controllers\Api\Logs
-     */
-    Route::prefix('log')->namespace('Log')->group(base_path('routes/api/log.php'));
-
-    /**
-     * @package \App\Http\Controllers\Api\System
-     */
-    Route::prefix('system')->namespace('System')->group(base_path('routes/api/system.php'));
 
     /**
      * @package \App\Http\Controllers\Api\Master
