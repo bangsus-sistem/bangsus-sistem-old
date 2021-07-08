ad53858658bc5bb64b6dd75be0e46599f2c27ba9
artevance <agustinus.michael29@gmail.com>
2021-07-08 12:50:00 +0700

    Remove unused files

--- routes/storage/index.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-use Illuminate\Support\Facades\Route;
-
-Route::get('image/{id}', 'ImageController@show')->name('storage.image');
-Route::get('file/{id}', 'FileController@show')->name('storage.file');
