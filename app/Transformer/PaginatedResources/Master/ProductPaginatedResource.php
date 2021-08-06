3010c1de583bbd169a0275d47b09ccb7ffaa64c9
artevance <agustinus.michael29@gmail.com>
2021-08-06 12:15:44 +0700

    Finish product transformers

--- app/Transformer/PaginatedResources/Master/ProductPaginatedResource.php
+++ app/Transformer/PaginatedResources/Master/ProductPaginatedResource.php
@@ -20,19 +20,9 @@ class ProductPaginatedResource extends PaginatedResource
             'product_category' => wbcm_paginated_resource('master.product_category'),
             'image' => wbcm_paginated_resource('storage.image'),
             'unit' => wbcm_paginated_resource('master.unit'),
-            'second_unit' => wbcm_paginated_resource('master.unit'),
-            'second_ratio' => (float) $this->second_ratio,
-            'third_unit' => wbcm_paginated_resource('master.unit'),
-            'third_ratio' => (float) $this->third_ratio,
-            'fourth_unit' => wbcm_paginated_resource('master.unit'),
-            'fourth_ratio' => (float) $this->fourth_ratio,
-            'fifth_unit' => wbcm_paginated_resource('master.unit'),
-            'fifth_ratio' => (float) $this->fifth_ratio,
             'active' => (bool) $this->active,
             'locked' => (bool) $this->locked,
             'monitor_stock' => (bool) $this->monitor_stock,
-            'purchase' => (bool) $this->purchase,
-            'sales' => (bool) $this->sales,
         ];
     }
 }
